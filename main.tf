ab4c035918838398c909448fa755ef42403a6349
Marcio Goda <marciogoda@gmail.com>
2023-12-04 12:26:33 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
 
 module "ecs_update_monitor" {
   source  = "mergermarket/ecs-update-monitor/acuris"
-  version = "2.3.4"
+  version = "2.3.5"
 
   cluster = var.ecs_cluster
   service = module.service.name
