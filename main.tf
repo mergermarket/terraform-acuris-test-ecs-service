7841a97b4b41b61930425f64fae514c277b94e69
Keir Badger <keir.badger@iongroup.com>
2024-04-04 09:13:39 +0000

    Update service to use capacity providers

--- main.tf
+++ main.tf
@@ -14,9 +14,23 @@ module "ecs_update_monitor" {
   timeout = var.deployment_timeout
 }
 
+locals {
+  capacity_providers = var.image_build_details["buildx"] == "true" && strcontains(var.image_build_details["platform"], "arm64") ? [
+    {
+      capacity_provider = "${var.ecs_cluster}-native-scaling-graviton"
+      weight            = 1
+    },
+  ] : [
+    {
+      capacity_provider = "${var.ecs_cluster}-native-scaling"
+      weight            = 1
+    },
+  ]
+}
+
 module "service" {
   source  = "mergermarket/load-balanced-ecs-service-no-target-group/acuris"
-  version = "2.2.7"
+  version = "2.4.0"
 
   name                                  = local.full_service_name
   cluster                               = var.ecs_cluster
@@ -32,6 +46,7 @@ module "service" {
   network_configuration_security_groups = var.network_configuration_security_groups
   pack_and_distinct                     = var.pack_and_distinct
   health_check_grace_period_seconds     = var.health_check_grace_period_seconds
+  capacity_providers                    = local.capacity_providers
 }
 
 module "taskdef" {
